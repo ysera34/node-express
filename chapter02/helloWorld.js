f6bcfee0f38ce939c2b86a6fdb602ba0fcb1aa7d
ysera34 <ycyoon34@gmail.com>
2017-07-14 19:07:00 +0900

    first routing

--- chapter02/helloWorld.js
+++ chapter02/helloWorld.js
@@ -1,8 +1,22 @@
 var http = require('http');
 
 http.createServer(function(req, res){
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello world!');
+
+  var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+  switch (path) {
+    case '':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('Homepage');
+      break;
+    case '/about':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('about');
+      break;
+    default:
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('not found');
+      break;
+  }
 }).listen(3000);
 
 console.log('Server started on localhost:3000; press Ctrl-C to terminate.');
