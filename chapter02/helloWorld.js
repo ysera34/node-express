487622bdb7f26345ae42f054b6d0e7561488f028
ysera34 <ycyoon34@gmail.com>
2017-07-14 19:22:58 +0900

    public static

--- chapter02/helloWorld.js
+++ chapter02/helloWorld.js
@@ -1,20 +1,34 @@
-var http = require('http');
+var http = require('http'),
+    fs = require('fs');
+
+function serveStaticFile(res, path, contentType, responseCode) {
+  if (!responseCode) responseCode = 200;
+  fs.readFile(__dirname + path, function(err, data) {
+    if (err) {
+      res.writeHead(500, {'Content-Type': 'text/plain'});
+      res.end('500 - Internal Error');
+    } else {
+      res.writeHead(responseCode, {'Content-Type': contentType});
+      res.end(data);
+    }
+  });
+}
 
 http.createServer(function(req, res){
 
   var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
   switch (path) {
     case '':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('Homepage');
+      serveStaticFile(res, '/public/home.html', 'text/html');
       break;
     case '/about':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('about');
+      serveStaticFile(res, '/public/about.html', 'text/html');
+      break;
+    case '/img/logo.png':
+      serveStaticFile(res, '/public/img/logo.png', 'image/png');
       break;
     default:
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('not found');
+      serveStaticFile(res, '/public/404.html', 'text/html', 404);
       break;
   }
 }).listen(3000);
