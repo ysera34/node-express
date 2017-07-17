6587f42571fe3411f6df488bac809da207191d60
ysera34 <ycyoon34@gmail.com>
2017-07-17 11:53:05 +0900

    express-handlebars

--- chapter03/index.js
+++ chapter03/index.js
@@ -1,29 +1,40 @@
 var express = require('express');
 
 var app = express();
+
+// handlebar view engine config
+var handlebars = require('express-handlebars')
+  .create({defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(req, res){
-  res.type('text/plain');
-  res.send('home');
+  // res.type('text/plain');
+  // res.send('home');
+  res.render('home');
 });
 
 app.get('/about', function(req, res){
-  res.type('text/plain');
-  res.send('about')
+  // res.type('text/plain');
+  // res.send('about');
+  res.render('about');
 });
 
 app.use(function(req, res){
-  res.type('text/plain');
+  // res.type('text/plain');
   res.status(404);
-  res.send('404 - Not Found');
+  // res.send('404 - Not Found');
+  res.render('404');
 });
 
 app.use(function(err, req, res, next){
   console.error(err.stack);
-  res.type('text/plain');
+  // res.type('text/plain');
   res.status(500);
-  res.send('500 - Server Error');
+  // res.send('500 - Server Error');
+  res.render('500');
 });
 
 app.listen(app.get('port'), function(){
---
+++ chapter03/views/about.handlebars
@@ -0,0 +1,1 @@
+<h1>Welcome to About</h1>
