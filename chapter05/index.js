7ec21fcc2e16dbd65d1a8b5d36b8c098b6512615
ysera34 <ycyoon34@gmail.com>
2017-07-19 10:48:21 +0900

    page test with mocha, chai and tests-script

--- chapter05/index.js
+++ chapter05/index.js
@@ -13,14 +13,21 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+// set showTests, context property if the querystring contains test=1
+app.use(function(req, res, next){
+  res.locals.showTests = app.get('env') !== 'production'
+    && req.query.test === '1';
+    next();
+});
 app.get('/', function(req, res){
   res.render('home');
 });
 
 app.get('/about', function(req, res){
-  // var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-  // res.render('about', {fortune : randomFortune});
-  res.render('about', {fortune : fortune.getFortune()});
+  res.render('about', {
+    fortune : fortune.getFortune(),
+    pageTestScript: '/qa/tests-about.js'
+  });
 });
 
 // 404 catch-all handler (middleware)
