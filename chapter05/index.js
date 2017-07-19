ffa09a1e54c3f80c77f3f34350629a4da29d76b4
ysera34 <ycyoon34@gmail.com>
2017-07-19 10:12:04 +0900

    chapter05, and install mocha

---
+++ chapter05/index.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var fortune = require('./lib/fortune.js');
+
+var app = express();
+
+// handlebar view engine config
+var handlebars = require('express-handlebars')
+  .create({defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.render('home');
+});
+
+app.get('/about', function(req, res){
+  // var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+  // res.render('about', {fortune : randomFortune});
+  res.render('about', {fortune : fortune.getFortune()});
+});
+
+// 404 catch-all handler (middleware)
+app.use(function(req, res){
+  res.status(404);
+  res.render('404');
+});
+
+// 500 catch-all handler (middleware)
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') +
+   '; press Ctrl + C to terminate.');
+});
+
+// var fortunes = [
+//   "Conquer your fears or they wills conquer you.",
+//   "Rivers need springs.",
+//   "Do not fear what you don't know.",
+//   "You will have a pleasant surprise.",
+//   "Whenever possible, keep it simple.",
+// ];
