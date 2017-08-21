fbebe50dcae5be5da228bdad00363925a60667c9
ysera34 <ycyoon34@gmail.com>
2017-08-21 16:12:18 +0900

    partials handlebars

--- chapter07/index.js
+++ chapter07/index.js
@@ -26,6 +26,14 @@ app.use(function(req, res, next){
   res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
     next();
 });
+
+// middleware to add weather data to context
+app.use(function(req, res, next){
+  if (!res.locals.partials) res.locals.partials = {};
+  res.locals.partials.weatherContext = getWeatherData();
+  next();
+});
+
 app.get('/', function(req, res){
   res.render('home');
 });
@@ -66,3 +74,31 @@ app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost:' + app.get('port') +
    '; press Ctrl + C to terminate.');
 });
+
+function getWeatherData() {
+  return {
+    locations: [
+      {
+        name: 'Portland',
+        forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+        weather: 'Overcast',
+        temp: '54.1 F (12.3 C)',
+      },
+      {
+          name: 'Bend',
+          forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+          iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+          weather: 'Partly Cloudy',
+          temp: '55.0 F (12.8 C)',
+      },
+      {
+          name: 'Manzanita',
+          forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+          iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+          weather: 'Light Rain',
+          temp: '55.0 F (12.8 C)',
+      },
+    ],
+  };
+}
