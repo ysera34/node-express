28cf59f208bcfa6a79ccd24a6fdaaec1104a3f5b
ysera34 <ycyoon34@gmail.com>
2017-08-28 19:52:37 +0900

    use domains for better error handling

--- chapter12/index.js
+++ chapter12/index.js
@@ -23,6 +23,54 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
+// use domains for better error handling
+app.use(function(req, res, next){
+  // create a domain for this request
+  var domain = require('domain').create();
+  // handle errors on this domain
+  domain.on('error', function(err){
+    console.error('DOMAIN ERROR CAUGHT\n', err.stack);
+    try {
+      // failsafe shutdown in 5 seconds
+      setTimeout(function(){
+        console.error('Failsafe shutdown.');
+        process.exit(1);
+      }, 5000);
+
+      // disconnect from the cluster
+      var worker = require('cluster').worker;
+      if (worker) worker.disconnect();
+
+      // stop taking new requests
+      server.close();
+
+      try {
+        // attempt to use Express error route
+        next(err);
+      } catch (e) {
+        // if Express error route failed, try plain Node response
+        console.error('Express error mechanism failed.\n', e.stack);
+        res.statusCode = 500;
+        res.setHeader('content-type', 'text/plain');
+        res.end('Server error');
+      } finally {
+
+      }
+    } catch (e) {
+      console.error('Unable to send 500 response.\n', e.stack);
+    } finally {
+
+    }
+  });
+
+  // add the request and response objects to the domain
+  domain.add(req);
+  domain.add(res);
+
+  // execute the rest of the request chain in the domain
+  domain.run(next);
+});
+
 app.use(express.static(__dirname + '/public'));
 
 // body parser
@@ -47,6 +95,7 @@ app.use(function(req, res, next) {
   var cluster = require('cluster');
   if (cluster.isWorker)
     console.log('Worker %d received request', cluster.worker.id);
+  next();
 });
 
 // cookie credentials, session configuration
@@ -260,6 +309,12 @@ app.post('/cart/checkout', function(req, res){
       html);
   });
   res.render('cart-thank-you', {cart: cart});
+});
+
+app.get('/epic-fail', function(req, res){
+  process.nextTick(function(){
+    throw new Error('Kaboom!');
+  });
 });
 
 // 404 catch-all handler (middleware)
