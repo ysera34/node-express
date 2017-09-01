fb1837c8e1d8381ad09a3c6ef804caffb1936ab8
ysera34 <ycyoon34@gmail.com>
2017-09-01 16:17:43 +0900

    file system persistence

--- chapter13/index.js
+++ chapter13/index.js
@@ -3,6 +3,7 @@ var express = require('express');
 var fortune = require('./lib/fortune.js');
 var formidable = require('formidable');
 var credentials = require('./credentials.js');
+var fs = require('fs');
 
 var app = express();
 var emailService = require('./lib/email.js')(credentials);
@@ -253,6 +254,44 @@ app.post('/contest/vacation-photo/:year/:month', function(req, res){
     console.log('received files: ' + files);
     console.log(files);
     res.redirect(303, '/thank-you');
+  });
+});
+
+// make sure data directory exists
+var dataDir = __dirname + '/data';
+var vacationPhotoDir = dataDir + '/vacation-photo';
+if (!fs.existsSync(dataDir)) fs.mkdirSync(dataDir);
+if (!fs.existsSync(vacationPhotoDir)) fs.mkdirSync(vacationPhotoDir);
+
+function saveContestEntry(contestName, email, year, month, photoPath) {
+  //TODO... this will come later
+}
+
+app.post('/contest/vacation-photo/:year/:month', function(req, res){
+  var form = new formidable.IncomingForm();
+  form.parse(req, function(err, fields, files){
+    if (err) {
+      req.session.flash = {
+        type: 'danger',
+        intro: 'Oops!',
+        message: 'There was an error processing your submission. ' +
+          'Please try again.'
+      };
+      return res.redirect(303, '/contest/vacation-photo');
+    }
+    var photo = files.photo;
+    var dir = vacationPhotoDir + '/' + Date.now();
+    var path = dir + '/' + photo.name;
+    fs.mkdirSync(dir);
+    fs.renameSync(photo.path, dir + '/' + photo.name);
+    saveContestEntry('vacation-photo', fields.email,
+      req.params.year, req.params.month, path);
+    req.session.flash = {
+      type: 'success',
+      intro: 'Good Luck',
+      message: 'You have been entered into the contest.',
+    };
+    return res.redirect(303, '/contest/vacation-photo/entries');
   });
 });
 
