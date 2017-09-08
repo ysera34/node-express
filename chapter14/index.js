a46d92a81f6ab5bfde8e652e8f206bdbe52c6386
ysera34 <ycyoon34@gmail.com>
2017-09-08 11:50:59 +0900

    automatically rendering views

--- chapter14/index.js
+++ chapter14/index.js
@@ -550,6 +550,23 @@ app.get('/epic-fail', function(req, res){
   });
 });
 
+// add support for auto views
+var autoViews = {};
+
+app.use(function(req, res, next) {
+  var path = req.path.toLowerCase();
+  // check cache; if it's there, render the views
+  if (autoViews[path]) return res.render(autoViews[path]);
+  // if it's not in the cache, see if there's
+  // a .handlebars file that matches
+  if (fs.existsSync(__dirname + '/views' + path + '.handlebars')) {
+    autoViews[path] = path.replace(/^\//, '');
+    return res.render(autoViews[path]);
+  }
+  // no view found, pass on to 404 handler
+  next();
+})
+
 // 404 catch-all handler (middleware)
 app.use(function(req, res){
   res.status(404);
